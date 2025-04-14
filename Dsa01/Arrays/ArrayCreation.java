5e5ebc35505b23e35dc8901e12e5f100342a0ca7
dubenikhil461 <nikhildubey461@gmail.com>
2025-04-14 16:00:09 +0530

    updates

---
+++ Dsa01/Arrays/ArrayCreation.java
@@ -0,0 +1,15 @@
+public class ArrayCreation {
+    public static void main(String[] args) {
+        // datatype name[] = new datatype[size]
+        int marks[] = new int[20];
+        marks[0] = 23;
+        marks[5]= 43;
+        System.out.println(marks[0]);
+
+        //WITHOUT SIZE
+        int number[] = {1,2,4,5,6};  // size is defined 5 
+       
+        // String type array
+        String fruits[] = {"apple","banana","mango"};
+    }
+}
